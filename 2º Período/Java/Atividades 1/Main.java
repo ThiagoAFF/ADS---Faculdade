d3bfcb5226622bfa56098976f64a43d339f195b0
ThiagoAFF <thiagoafferreira127@gmail.com>
2026-08-18 16:37:06 -0300

    Adicionei o six seven aqui

--- 2º Período/Java/Atividades 1/Main.java
+++ 2º Período/Java/Atividades 1/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Six Seven é o GOAT Sigma boy!!! 67 67 67 67 67 67 67 67");
     }
 }
