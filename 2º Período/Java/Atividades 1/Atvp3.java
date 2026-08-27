d51d05b3a532a39a35b18a2b8fbf151600b2f31a
ThiagoAFF <thiagoafferreira127@gmail.com>
2026-08-27 20:38:48 -0300

    Atualização do código, para o correto

--- 2º Período/Java/Atividades 1/Atvp3.java
+++ 2º Período/Java/Atividades 1/Atvp3.java
@@ -10,14 +10,25 @@ public class Atvp3 {
                 Menu
                 1. Calcular área de quadrado
                 2. Calcular área do círculo
-                """;
+
+                Escolha:  """;
 
         System.out.print(menu);
         int esc = choice.nextInt();
 
         switch (esc) {
             case 1:
-                
+                System.out.print("Digite o tamanho do lado do quadrado: ");
+                float l = choice.nextInt();
+
+                System.out.print("Área do quadrado: " + l*l);
+                break;
+            case 2:
+                System.out.print("Digito o valor do raio do círculo: ");
+                float r = choice.nextInt();
+
+                System.out.println("Área do círculo: " + 3.14*(r+r));
+                break;
         }
     }
 }
